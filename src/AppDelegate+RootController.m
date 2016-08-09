3972ef4c6b93449ee69cc18048d0d1096d89ca83
Nikolay Chaban <nchaban.kh@gmail.com>
2016-08-09 13:21:46 +0300

    - added AppDelegate category

---
+++ src/AppDelegate+RootController.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+RootController.m
+//  TookTODO
+//
+//  Created by Глеб on 09.08.16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import "AppDelegate+RootController.h"
+
+@implementation AppDelegate (RootController)
+
+@end
