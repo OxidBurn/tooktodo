b0d2c20832e5c3a2e50d3ce91d55c2317db23981
Nikolay Chaban <nchaban.kh@gmail.com>
2016-08-16 19:12:53 +0300

    - added CellID in user notification table view

--- src/UI/User/UserNotificationModel.m
+++ src/UI/User/UserNotificationModel.m
@@ -9,6 +9,10 @@
 #import "UserNotificationModel.h"
 @import UIKit;
 
+static NSString* CellID = @"CellID";
+static NSString* LabelTextKey = @"LabelTextKey";
+static NSString* SwitchTagKey = @"SwitchTagKey";
+
 @interface UserNotificationModel() <UITableViewDelegate, UITableViewDataSource>
 
 // properties
@@ -22,6 +26,34 @@
 @implementation UserNotificationModel
 
 #pragma mark - Properties -
+
+- (NSArray*) tableRowsData
+{
+    if ( _tableRowsData == nil )
+    {
+        _tableRowsData = @[@[@{LabelTextKey: @"Все сообщения",
+                               SwitchTagKey: @1},
+                             @{LabelTextKey: @"Где я учавствую",
+                               SwitchTagKey: @2},
+                             @{LabelTextKey: @"Где я ответственный или утверждающий",
+                               SwitchTagKey: @3}],
+                           
+                           @[@{LabelTextKey: @"Сообщения в ленту",
+                               SwitchTagKey: @4},
+                             @{LabelTextKey: @"Задачи",
+                               SwitchTagKey: @5},
+                             @{LabelTextKey: @"Документы",
+                               SwitchTagKey: @6},
+                             @{LabelTextKey: @"Команды",
+                               SwitchTagKey: @7},
+                             @{LabelTextKey: @"Профиль проекта",
+                               SwitchTagKey: @8},
+                             @{LabelTextKey: @"Системные",
+                               SwitchTagKey: @9}],
+                           ];
+    }
+    return _tableRowsData;
+}
 
 #pragma mark - UITableViewDataSource methods -
 
