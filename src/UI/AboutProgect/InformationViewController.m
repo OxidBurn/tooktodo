2e7d70ebfb52456aa10ccebd2fc1a087c5abf36c
Nikolay Chaban <nchaban.kh@gmail.com>
2016-08-23 18:06:32 +0300

    - added about project, system, information, role controllers - added classes for this controllers - added asset for button add - added font UI-Text-Medium

---
+++ src/UI/AboutProgect/InformationViewController.m
@@ -0,0 +1,30 @@
+//
+//  InformationInfoViewController.m
+//  TookTODO
+//
+//  Created by Chaban Nikolay on 8/23/16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import "InformationViewController.h"
+
+@interface InformationViewController ()
+
+@end
+
+@implementation InformationViewController
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void) didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ src/UI/AboutProgect/RoleInfoViewController.m
@@ -0,0 +1,30 @@
+//
+//  RoleInfoViewController.m
+//  TookTODO
+//
+//  Created by Chaban Nikolay on 8/23/16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import "RoleInfoViewController.h"
+
+@interface RoleInfoViewController ()
+
+@end
+
+@implementation RoleInfoViewController
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void) didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
