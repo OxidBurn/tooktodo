2350faf10a397a5770163f0c9bae612dbaa51670
Nikolay Chaban <nchaban.kh@gmail.com>
2016-09-13 17:53:05 +0300

    - added design changes with add task controller from task option screen - added title for navigation bar to add task controller

--- src/Modules/Tasks/AddTask/AddTaskViewController.m
+++ src/Modules/Tasks/AddTask/AddTaskViewController.m
@@ -14,16 +14,75 @@
 
 @implementation AddTaskViewController
 
+#pragma mark - Life cycle -
+
+- (void) loadView
+{
+    [super loadView];
+    
+    [self twoLineTitleView];
+}
+
 - (void) viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
+
+#pragma mark - Memory managment -
 
 - (void) didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Internal methods -
+
+- (UIView*) twoLineTitleView
+{
+    UIFont* customFont            = [UIFont fontWithName: @"SFUIText-Regular"
+                                                    size: 14.0f];
+    UIFont* customFontForSubTitle = [UIFont fontWithName: @"SFUIText-Regular"
+                                                    size: 13.0f];
+    
+    UILabel* titleLabel        = [[UILabel alloc] initWithFrame: CGRectMake(0, 0, 0, 0)];
+    titleLabel.backgroundColor = [UIColor clearColor];
+    titleLabel.textColor       = [UIColor whiteColor];
+    titleLabel.font            = customFont;
+    titleLabel.textAlignment   = NSTextAlignmentCenter;
+    titleLabel.text            = @"НОВАЯ ЗАДАЧА";
+    [titleLabel sizeToFit];
+    
+    UILabel* subTitleLabel        = [[UILabel alloc] initWithFrame: CGRectMake(0, 17, 0, 0)];
+    subTitleLabel.backgroundColor = [UIColor clearColor];
+    subTitleLabel.textColor       = [UIColor whiteColor];
+    subTitleLabel.font            = customFontForSubTitle;
+    subTitleLabel.textAlignment   = NSTextAlignmentCenter;
+//    subTitleLabel.text            = [self.viewModel getProjectName];
+    [subTitleLabel sizeToFit];
+    
+    UIView* twoLineTitleView = [[UIView alloc] initWithFrame: CGRectMake(0, 0, MAX(subTitleLabel.frame.size.width, titleLabel.frame.size.width), 32)];
+    
+    [twoLineTitleView addSubview: titleLabel];
+    [twoLineTitleView addSubview: subTitleLabel];
+    
+    float widthDiff = subTitleLabel.frame.size.width - titleLabel.frame.size.width;
+    
+    if (widthDiff > 0)
+    {
+        CGRect frame     = titleLabel.frame;
+        frame.origin.x   = widthDiff / 2;
+        titleLabel.frame = CGRectIntegral(frame);
+    }
+    else
+    {
+        CGRect frame        = subTitleLabel.frame;
+        frame.origin.x      = fabsf(widthDiff) / 2;
+        subTitleLabel.frame = CGRectIntegral(frame);
+    }
+    
+    self.navigationItem.titleView = twoLineTitleView;
+    
+    return twoLineTitleView;
 }
 
 
