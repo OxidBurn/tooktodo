05f397d34ab52a59473b79c945a2bf125d52d678
Nikolay Chaban <nchaban.kh@gmail.com>
2016-09-16 17:53:42 +0300

    - added add massage view controller - added terms screen - added add massage screen

---
+++ src/UI/Custom Components/AddMassageViewController.m
@@ -0,0 +1,31 @@
+//
+//  AddMassageViewController.m
+//  TookTODO
+//
+//  Created by Chaban Nikolay on 9/16/16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import "AddMassageViewController.h"
+
+@interface AddMassageViewController ()
+
+//Properties
+
+//Methods
+
+@end
+
+@implementation AddMassageViewController
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void) didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
