e3b7a22bfc629b8ec8b148dce7e8eb69135a2828
Nikolay Chaban <nchaban.kh@gmail.com>
2016-10-05 14:29:22 +0300

    - display levels in tableview

---
+++ pod/ProjectTaskRoom+CoreDataClass.h
@@ -0,0 +1,22 @@
+//
+//  ProjectTaskRoom+CoreDataClass.h
+//  
+//
+//  Created by Lera on 05.10.16.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class ProjectTask, ProjectTaskMapContour, ProjectTaskRoomLevel;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProjectTaskRoom : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "ProjectTaskRoom+CoreDataProperties.h"
