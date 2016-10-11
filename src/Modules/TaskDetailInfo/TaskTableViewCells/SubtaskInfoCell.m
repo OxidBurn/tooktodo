cf68da67928d0a5a592a6bf3c8c7a0a7c1a396a9
Nikolay Chaban <nchaban.kh@gmail.com>
2016-10-11 18:49:42 +0300

    - restructured project

---
+++ src/Modules/TaskDetailInfo/TaskTableViewCells/SubtaskInfoCell.m
@@ -0,0 +1,92 @@
+//
+//  SubtaskInfoCell.m
+//  TookTODO
+//
+//  Created by Глеб on 05.10.16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import "SubtaskInfoCell.h"
+
+// Classes
+#import "AvatarImageView.h"
+#import "TaskMarkerComponent.h"
+#import "StatusMarkerComponent.h"
+
+@interface SubtaskInfoCell()
+
+// outlets
+@property (weak, nonatomic) IBOutlet UILabel*               taskTermsLabel;
+@property (weak, nonatomic) IBOutlet UIImageView*           isExpiredImageView;
+@property (weak, nonatomic) IBOutlet StatusMarkerComponent* taskStatusMark;
+@property (weak, nonatomic) IBOutlet UILabel*               workAreaShortTitle;
+@property (weak, nonatomic) IBOutlet UIButton*              accessBtn;
+@property (weak, nonatomic) IBOutlet UIButton*              statusBtn;
+@property (weak, nonatomic) IBOutlet UILabel*               taskTitleLabel;
+@property (weak, nonatomic) IBOutlet UIImageView*           changeStatusMarkImageView;
+@property (weak, nonatomic) IBOutlet UILabel*               statusDescriptionLabel;
+@property (weak, nonatomic) IBOutlet UIImageView*           statusExpandedImageView;
+@property (weak, nonatomic) IBOutlet TaskMarkerComponent*   subtaskMark;
+@property (weak, nonatomic) IBOutlet TaskMarkerComponent*   attachmentsMark;
+@property (weak, nonatomic) IBOutlet TaskMarkerComponent*   commentsMark;
+@property (weak, nonatomic) IBOutlet UILabel*               roomNumberLabel;
+@property (weak, nonatomic) IBOutlet UIImageView*           roomNumberMarkImageView;
+
+
+
+// properties
+
+
+// methods
+
+- (IBAction) onHiddenTaskBtn:   (UIButton*) sender;
+
+- (IBAction) onChangeStatusBtn: (UIButton*) sender;
+
+@end
+
+@implementation SubtaskInfoCell
+
+#pragma mark - Actions -
+
+- (IBAction) onHiddenTaskBtn: (UIButton*) sender
+{
+    
+}
+
+- (IBAction) onChangeStatusBtn: (UIButton*) sender
+{
+    
+}
+
+#pragma mark - Public -
+
+- (void) fillCellWithContent: (TaskRowContent*) content
+{
+    self.taskTitleLabel.text   = content.taskTitle;
+    self.roomNumberLabel.text = [NSString stringWithFormat: @"%ld", content.roomNumber];
+    [self.accessBtn setBackgroundImage: [self getBackgroundImageForTaskAccess: content.isHiddenTask]
+                                      forState: UIControlStateNormal];
+}
+
+#pragma mark - Helpers -
+
+- (UIImage*) getBackgroundImageForTaskAccess: (BOOL) isHiddenTask
+{
+    UIImage* image;
+    
+    if ( isHiddenTask )
+    {
+        image = [UIImage imageNamed: @"closedEyes"];
+    }
+    else
+    {
+        image = [UIImage imageNamed: @"Eye"];
+    }
+    
+    return image;
+}
+
+
+
+@end
