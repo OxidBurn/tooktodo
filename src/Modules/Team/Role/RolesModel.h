74a082703d5a804b5f4cbe87a72556189a9d25d2
Nikolay Chaban <nchaban.kh@gmail.com>
2016-11-02 20:27:45 +0200

    - changed cell type for RolesViewController

---
+++ src/Modules/Team/Role/RolesModel.h
@@ -0,0 +1,31 @@
+//
+//  RolesModel.h
+//  TookTODO
+//
+//  Created by Lera on 02.11.16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ReactiveCocoa.h"
+
+//Classes
+#import "InviteInfo.h"
+#import "RolesService.h"
+#import "ProjectRoles+CoreDataProperties.h"
+
+@interface RolesModel : NSObject
+
+- (RACSignal*) updateRolesInfo;
+
+- (NSUInteger) countOfRows;
+
+- (NSString*) getRoleNameByIndex: (NSIndexPath*) indexPath;
+
+- (BOOL) handleSelectedStateForRole: (NSIndexPath*) indexPath;
+
+- (void) updateLastIndexPath: (NSIndexPath*) indexPath;
+
+- (ProjectRoles*) getSelectedItem;
+
+@end
