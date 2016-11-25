b1431269e9350b65deb7d0da3ae493b052a5389c
Nikolay Chaban <nchaban.kh@gmail.com>
2016-11-25 15:04:19 +0200

    - added segue to approvers controller

--- src/Modules/TaskDetailInfo/MembersFromCollection/ApproverViewController.m
+++ src/Modules/TaskDetailInfo/MembersFromCollection/ApproverViewController.m
@@ -25,20 +25,20 @@
 
 @implementation ApproverViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction) onDone: (UIBarButtonItem*) sender
+{
+    
 }
 
-
-- (IBAction)onDone:(UIBarButtonItem *)sender {
-}
-
-- (IBAction)onBackBtn:(UIBarButtonItem *)sender {
+- (IBAction) onBackBtn: (UIBarButtonItem*) sender
+{
+    [self.navigationController popViewControllerAnimated: YES];
 }
 @end
