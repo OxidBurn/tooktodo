e3b40b8cb0107267b38c3eba9cceb6fb16c0811b
Nikolay Chaban <nchaban.kh@gmail.com>
2016-11-25 13:16:55 +0200

    - added controller for showing approvers - added outlets to tableviewcell

---
+++ inc/Modules/TaskDetailInfo/MembersFromCollection/ApproverTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ApproverTableViewCell.h
+//  TookTODO
+//
+//  Created by Lera on 25.11.16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApproverTableViewCell : UITableViewCell
+
+@end
---
+++ src/Modules/TaskDetailInfo/MembersFromCollection/ApproverTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ApproverTableViewCell.m
+//  TookTODO
+//
+//  Created by Lera on 25.11.16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import "ApproverTableViewCell.h"
+
+@interface ApproverTableViewCell()
+
+// properties
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImgView;
+@property (weak, nonatomic) IBOutlet UIImageView *checkMarkImgView;
+@property (weak, nonatomic) IBOutlet UILabel *userNameImgView;
+
+// methods
+
+@end
+
+@implementation ApproverTableViewCell
+
+@end
