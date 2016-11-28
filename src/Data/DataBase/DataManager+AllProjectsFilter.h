a7ded3de895fd9a0e8ffd17db8151409c7cbec3b
Nikolay Chaban <nchaban.kh@gmail.com>
2016-11-28 06:00:57 +0200

    - added category for working with all projects filter

---
+++ src/Data/DataBase/DataManager+AllProjectsFilter.h
@@ -0,0 +1,18 @@
+//
+//  DataManager+AllProjectsFilter.h
+//  TookTODO
+//
+//  Created by Nikolay Chaban on 11/28/16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import "DataManager.h"
+
+// Classes
+
+
+@interface DataManager (AllProjectsFilter)
+
+//- (void) persistAllProjectsFilter
+
+@end
---
+++ src/Data/DataBase/DataManager+AllProjectsFilter.m
@@ -0,0 +1,13 @@
+//
+//  DataManager+AllProjectsFilter.m
+//  TookTODO
+//
+//  Created by Nikolay Chaban on 11/28/16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import "DataManager+AllProjectsFilter.h"
+
+@implementation DataManager (AllProjectsFilter)
+
+@end
