e358ab6933dc5dd1bf5702a99b19f8078aa73a09
Nikolay Chaban <nchaban.kh@gmail.com>
2016-12-14 20:00:59 +0200

    - implemented base logic of 'CancelRequest' module

--- src/Modules/TaskDetailInfo/CancelRequest/CancelRequestViewModel.m
+++ src/Modules/TaskDetailInfo/CancelRequest/CancelRequestViewModel.m
@@ -8,6 +8,40 @@
 
 #import "CancelRequestViewModel.h"
 
+// Classes
+#import "CancelRequestModel.h"
+
+@interface CancelRequestViewModel()
+
+// properties
+@property (strong, nonatomic) CancelRequestModel* model;
+
+// methods
+
+
+@end
+
 @implementation CancelRequestViewModel
+
+
+#pragma mark - Properties -
+
+- (CancelRequestModel*) model
+{
+    if ( _model == nil )
+    {
+        _model = [CancelRequestModel new];
+    }
+    
+    return _model;
+}
+
+
+#pragma mark - Public -
+
+- (void) sendRequestLetter: (NSString*) letterText
+{
+    [self.model sendRequestLetter: letterText];
+}
 
 @end
