bfa9d7027984374682dbaec6988c79a8b3970bf2
Nikolay Chaban <nchaban.kh@gmail.com>
2016-12-22 14:49:49 +0200

    - setup new task log cells: comment, document and types

---
+++ src/UI/Base/ViewWithBorder.h
@@ -0,0 +1,13 @@
+//
+//  ViewWithBorder.h
+//  TookTODO
+//
+//  Created by Nikolay Chaban on 12/22/16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewWithBorder : UIView
+
+@end
