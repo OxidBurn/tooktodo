0816aab766a42981c2ede36f248dcf37a92ce880
Nikolay Chaban <nchaban.kh@gmail.com>
2016-12-22 19:56:30 +0200

    - added headerView for search bar

---
+++ src/Modules/Tasks/SearchBarHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  SearchBarHeaderView.h
+//  TookTODO
+//
+//  Created by Nikolay Chaban on 22.12.16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchBarHeaderView : UIView
+
+@end
